37e6c771fc0e784cb2748894eb3acbb8c6164517
nikoloz-tsiklauri <101364102+nikoloz-tsiklauri@users.noreply.github.com>
2022-09-24 13:46:12 +0400

    jgf

---
+++ day 1/lesson1.py
@@ -0,0 +1,7 @@
+about_me = "me mqvia"
+name = "nikolozi"
+my_age = "var 17 wlis, myavs"
+fm = 6
+bros = "dzma"
+education = "da vswavlob goal-oriented academy shi"
+print(about_me + " " + name + " " + my_age + " " + str(fm) + " " + bros + " " + education)
---
+++ day2/lesson2.py
@@ -0,0 +1,5 @@
+full_name = "nikoloz wiklauri"
+print(full_name[:8])
+print(full_name[8:])
+print(full_name[-4])
+print(full_name.replace("i", "#"))
